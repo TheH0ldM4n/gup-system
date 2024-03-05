d8c62ad628d10ea82152bed6c0d0b35ed2dfaa64
KuroKratos <=>
2024-03-05 10:20:11 +0100

    init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const DEBILUS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+DEBILUS.abilities = {
+  str: 'DEBILUS.Ability.Str.long',
+  dex: 'DEBILUS.Ability.Dex.long',
+  con: 'DEBILUS.Ability.Con.long',
+  int: 'DEBILUS.Ability.Int.long',
+  wis: 'DEBILUS.Ability.Wis.long',
+  cha: 'DEBILUS.Ability.Cha.long',
+};
+
+DEBILUS.abilityAbbreviations = {
+  str: 'DEBILUS.Ability.Str.abbr',
+  dex: 'DEBILUS.Ability.Dex.abbr',
+  con: 'DEBILUS.Ability.Con.abbr',
+  int: 'DEBILUS.Ability.Int.abbr',
+  wis: 'DEBILUS.Ability.Wis.abbr',
+  cha: 'DEBILUS.Ability.Cha.abbr',
+};
